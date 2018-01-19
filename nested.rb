2f3ee0034757275e8db049cb7da4fc6ff51e75ac
Melissa Nock <melnock@gmail.com>
2018-01-19 14:14:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]=.push("assembly")
+  programmer_hash[:dennis_ritchie][:languages]=dennis_programs_known
+  dennis_programs_known<<"Assembly"
   programmer_hash
 end
